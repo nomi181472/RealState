bfc6ff2dac9dc629023a66d9d5876b7bb49a7d47
Noman Ali <33200799+nomi181472@users.noreply.github.com>
2022-03-27 15:39:19 +0500

    realstate bootstrap added

---
+++ RealState/realstate/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using realstate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace realstate.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Random()
+        {
+            User user = new User() { Email = "myemail", Id = 1 };
+            return View(user);
+        }
+    }
+}
---
+++ RealState/realstate/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace realstate.Models
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public int Id { get; set; }
+    }
+}
